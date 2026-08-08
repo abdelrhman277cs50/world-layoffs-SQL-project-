58c98a0bb12e61dad52c20e139d63e36d20b5a38
Abdelrahman Mohamed ELSayed ELBayoumi <158166262+abdelrhman277cs50@users.noreply.github.com>
2026-08-08 20:33:49 +0300

    Data cleaning project.sql

--- Data cleaning project.sql
+++ Data cleaning project.sql
@@ -1,21 +1,21 @@
 -- Data cleaning
 
 select *
-from layoffs;
+from world_layoffs.layoffs;
 
 
-create table layoffs_staging
-like layoffs;
+create table world_layoffs.layoffs_staging
+like world_layoffs.layoffs;
 
 insert layoffs_staging
 select*
-from layoffs;
+from world_layoffs.layoffs;
 
 select *
-from layoffs_staging;
+from world_layoffs.layoffs_staging;
 
 -- Remove Duplicates 
-# First let's check for duplicates
+-- First let's check for duplicates
 
 with duplicates_cte as
 (
@@ -23,7 +23,7 @@ select*,
 row_number() over( 
 partition by company, location, industry, total_laid_off, percentage_laid_off, 'date',
 stage, country, funds_raised_millions) as row_num
-from layoffs_staging
+from world_layoffs.layoffs_staging
 )
 select *
 from duplicates_cte
@@ -36,29 +36,29 @@ DROP COLUMN row_num;
 -- these are the ones we want to delete where the row number is > 1
 
 select *
-from layoffs_staging
+from world_layoffs.layoffs_staging
 where company = 'casper';
 
-create table layoffs_staging2
-like layoffs_staging;
+create table world_layoffs.layoffs_staging2
+like world_layoffs.layoffs_staging;
 
-ALTER TABLE layoffs_staging2
+ALTER TABLE world_layoffs.layoffs_staging2
 ADD COLUMN row_num int;
 
-insert into layoffs_staging2
+insert into world_layoffs.layoffs_staging2
 select*,
 row_number() over( 
 partition by company, location, industry, total_laid_off, percentage_laid_off, 'date',
 stage, country, funds_raised_millions) as row_num
-from layoffs_staging;
+from world_layoffs.layoffs_staging;
 
 
 select *
-from layoffs_staging2
+from world_layoffs.layoffs_staging2
 where row_num >1;
 ## now that we have this we can delete rows were row_num is greater than 1
 delete 
-from layoffs_staging2
+from world_layoffs.layoffs_staging2
 where row_num >1;
 
 
@@ -66,13 +66,13 @@ where row_num >1;
 
 
 select company, trim(company)
-from layoffs_staging2;
+from world_layoffs.layoffs_staging2;
 
 update layoffs_staging2
 set company = trim(company);
 
 select  distinct industry
-from layoffs_staging2
+from world_layoffs.layoffs_staging2
 order by 1;
 
 update layoffs_staging2
@@ -80,7 +80,7 @@ set industry = 'Crypto'
 where industry like 'Crypto%';
 
 select  distinct country, trim(trailing "." from country)
-from layoffs_staging2
+from world_layoffs.layoffs_staging2
 order by 1;
 
 
@@ -89,31 +89,31 @@ set country =  trim(trailing "." from country)
 where country like 'United States%' ;
 
 select  distinct country
-from layoffs_staging2
+from world_layoffs.layoffs_staging2
 order by 1;
 
 ## update the date column from string to date format 
 
 select `date`,
 str_to_date(`date`, '%m/%d/%Y/')
-from layoffs_staging2;
+from world_layoffs.layoffs_staging2;
 
-update layoffs_staging2
+update world_layoffs.layoffs_staging2
 set `date` = str_to_date(`date`, '%m/%d/%Y/');
 
 select `date`
-from layoffs_staging2;
+from world_layoffs.layoffs_staging2;
 
-alter table layoffs_staging2
+alter table world_layoffs.layoffs_staging2
 modify column `date` date ; 
 
 select *
-from layoffs_staging2;
+from world_layoffs.layoffs_staging2;
 
  -- Look at null values 
  
 SELECT *
-FROM layoffs_staging2
+FROM world_layoffs.layoffs_staging2
 WHERE industry IS NULL 
 OR industry = ''
 ORDER BY industry;
@@ -121,39 +121,39 @@ ORDER BY industry;
 -- now we need to populate those nulls if possible
 
 SELECT *
-FROM layoffs_staging2
+FROM world_layoffs.layoffs_staging2
 WHERE industry IS NULL 
 OR industry = ''
 ORDER BY industry;
 
 
 select *
-from layoffs_staging2 t1
-JOIN layoffs_staging2 t2
+from world_layoffs.layoffs_staging2 t1
+JOIN world_layoffs.layoffs_staging2 t2
 	ON t1.company = t2.company;
 
-UPDATE layoffs_staging2 t1
-JOIN layoffs_staging2 t2
+UPDATE world_layoffs.layoffs_staging2 t1
+JOIN world_layoffs.layoffs_staging2 t2
 ON t1.company = t2.company
 SET t1.industry = t2.industry
 WHERE t1.industry IS NULL
 AND t2.industry IS NOT NULL;
 
  select *
- from layoffs_staging2
+ from world_layoffs.layoffs_staging2
  where total_laid_off is null
  and percentage_laid_off is null;
  
  # this is useless data we can delete it 
  
  delete
- from layoffs_staging2
+ from world_layoffs.layoffs_staging2
  where total_laid_off is null
  and percentage_laid_off is null;
  
  select *
- from layoffs_staging2;
+ from world_layoffs.layoffs_staging2;
  
- alter table layoffs_staging2
+ alter table world_layoffs.layoffs_staging2
  drop column row_num;
  
